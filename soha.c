87668cdff8f385072f7d1fbc737150bd76dc4cc7
Alan Gibran <lushwacka@comunidad.unam.mx>
2024-01-09 15:19:03 -0600

    Corrido

--- soha.c
+++ soha.c
@@ -1,3 +1,4 @@
-//just a nice comment
-// too delicious
-//Another comment
+So right now im in class, just fishing.
+They're singing to their favorite songs 
+and stuff.
+Just four weeks more to go.
