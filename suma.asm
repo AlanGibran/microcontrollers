2a1792876f1de70b93c3ff1ff85e42ea4ac4df18
Alan Gibrán López <89953522+AlanGibran@users.noreply.github.com>
2024-02-24 08:33:42 -0600

    Full documented

--- suma.asm
+++ suma.asm
@@ -1,13 +1,14 @@
 ; Project 2: Arithmetic using the Assembly Language
     .global main
-OS  .EQU 0x04 ; offset para rellenar
-ITER .EQU 10
-main
+; Offset that updates the pointer when assigning values to RAM
+OS  .EQU 0x04 
+ITER .EQU 10 ; Counter in decimal
 
-; 20 valores aleatorios a partir de 0x2000.0000
+main
+; Starting from 0x2000.0000
     MOVW R0, #0x0000
     MOVT R0, #0x2000
-
+; Store 20 random values (not really random)
     MOVW R1, #0x0100
     MOVT R1, #0x0302
     STR R1, [R0]
@@ -28,20 +29,20 @@ main
     MOVW R0, #0xFFFF ;[2000.0000 - 1] = 1FFF.FFFF
     MOVT R0, #0x1FFF
 
-	MOV R1, #0 ; Fetches each byte
-	MOV R2, #0 ; Final result
+	MOV R1, #0 ; Fetches each byte from RAM
+; R2 stores the 8-bit value from RAM 
+	MOV R2, #0 ; R2 = sum_0^9 X_i for each iteration
 
-	MOV  R10, #0; counter
+	MOV  R10, #0; Counter. if R10 = 10, exit SUM loop 
 
 SUM
-	ADD  R10, R10, #1 ; start with first iteration
-
+	ADD  R10, R10, #1 ; Start with the first iteration
 	LDRB R1, [R0, #1]!; {B} Just one byte is needed for each sum
-	ADDS R2, R1; store the updated result in R2
-	CMP R10, #ITER ; make sure to do this 10x
-	BNE SUM ; while not equal, go back and sum again
+	ADDS R2, R1; {s} activates flags. Store the updated result in R2
+	CMP R10, #ITER ; Make sure to do this 10x
+	BNE SUM ; While not equal, go back and sum again
 
-; store the sum result in a 32-bit format @0x2000.0020
+; Store the sum result in a 32-bit format @0x2000.0020
 	MOVW R0, #0x0020
 	MOVT R0, #0x2000
 	STR R2, [R0]
