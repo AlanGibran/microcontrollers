f113d8069492ba90bed3c3e628079a3a5c7981ee
Alan Gibrán López <89953522+AlanGibran@users.noreply.github.com>
2024-03-09 14:00:48 -0600

    Update leds.asm

--- leds.asm
+++ leds.asm
@@ -1,69 +1,68 @@
-;** EL PROGRAMA INICIALIZA EL PUERTO N COMO SALIDA y
-
 ;** 4 leds
 
 	.global main
 
-	.text ;define codigo del programa y lo ubica en flash
-
+	.text 
 SYSCTL_RCGCGPIO_R .field 0x400FE608,32 ; REGISTRO DEL RELOJ
 
-GPIO_PORTN_DIR_N .field 0x40064400, 32 ; Registro de Dirección N (input or output)
+
+GPIO_PORTN_DIR_N .field 0x40064400, 32 ; N (input or output)
 GPIO_PORTF_DIR_F .field 0x4005D400, 32 ; F
+
 
 ; Tristate, enable the use of the pin as a digital input or output
 ; (either GPIO or alternate function)
 GPIO_PORTN_DEN_N .field 0x4006451C, 32
 GPIO_PORTF_DEN_F .field 0x4005D51C, 32
 
-GPIO_PORTN_DATA_N .field 0x4006400C,32 ; Registro de Datos N (mask)
-GPIO_PORTF_DATA_F .field 0x4005D044,32
 
-LEDS .EQU 0x03 ; se asigna el valor a el simbolo LEDS
-LEDS_OFF .EQU 0X00 ; se asigna el valor 0x00 a el simbolo LEDS2
+GPIO_PORTN_DATA_N .field 0x4006400C,32 ; 0x0C bc 000.11[00]: PN1 y PN0 with mask
+GPIO_PORTF_DATA_F .field 0x4005D044,32 ; 0x44 bc 0100.01[00]: PF4 y PF0 with mask
 
-LEDS_F .EQU 0x11 ; 1001 PF[4] y PF[0]
+
+LEDS .EQU 0x03 ; 0000.0011b: PN[1] & PN[0]. No mask here
+LEDS_F .EQU 0x11 ; 00001.0001b: PF[4] y PF[0]
+LEDS_OFF .EQU 0X00 ; OFF
+
+
 
 loop_delay; (1)
-	SUBS R10, #0x1 ; Decrementar el contador (2)
-    BNE loop_delay ; Repetir si el contador no cero (3)
-    BX LR ; Regresa al programa principal
+    SUBS R10, #0x1 ; Count -= 1 (2)
+    BNE loop_delay ; Repeat if counter not zero  (3)
+    BX LR ; back to LR
 
 main
 
 	LDR R1,SYSCTL_RCGCGPIO_R
 	LDR R0, [R1]
-	ORR R0, R0, #0x1020 ; se valida el bit 12(puerto n) y 5(puerto f)
+	ORR R0, R0, #0x1020 ; PN=12 & PF=5; 1.0000.0010.0000
 	STR R0, [R1]
 	NOP
 	NOP
 
 ;-----------------------------------------------------------------
-	LDR R1,GPIO_PORTN_DIR_N ; Determina la dirección del puerto
-	MOV R0, #0xFF ; PN0 PN1 salidas
-	STR R0,[R1]
+	LDR R1,GPIO_PORTN_DIR_N ; input or output
+	MOV R0, #0x03 PN1 PN0
+	STR R0,[R1] ; write 0000.0011b onto DIR register
 
-	LDR R1, GPIO_PORTF_DIR_F ; Determina la dirección del puerto
-	MOV R0, #0xFF ; PF0 PF1 salidas
-	STR R0, [R1] ; PF4 y PF0
+	LDR R1, GPIO_PORTF_DIR_F ; input or output
+	MOV R0, #0x11 ; PF4 PF0
+	STR R0, [R1] ; write 0001.0001b onto DIR register
 ;-----------------------------------------------------------------
 
-	LDR R1, GPIO_PORTN_DEN_N ; Habilita al puerto digital N
-	MOV R0, #0xFF
+	LDR R1, GPIO_PORTN_DEN_N ; Digital Enable, same logic
+	MOV R0, #0x03
 	STR R0, [R1]
 
-	LDR R1, GPIO_PORTF_DEN_F ; Habilita al puerto digital F
-	MOV R0, #0xFF
-	STR R0, [R1]
+	LDR R1, GPIO_PORTF_DEN_F ; 
+	MOV R5, #0x11
+	STR R5, [R1]
 
-	MOV R5, #0xFF
 ;-----------------------------------------------------------------
 
-	LDR R1, GPIO_PORTN_DATA_N ; apunta al Puerto de datos N
-	AND R0, #LEDS ; (FF AND LEDS) = LEDS
+	LDR R1, GPIO_PORTN_DATA_N ; Gotta write ON/OFF here
 
-	LDR R3, GPIO_PORTF_DATA_F ; apunta al Puerto de datos N
-	AND R5, #LEDS_F ; (FF AND LEDS) = LEDS
+	LDR R3, GPIO_PORTF_DATA_F 
 
 salto
 	STR R0, [R1] ; Escribe en el registro del datos del Puerto N
